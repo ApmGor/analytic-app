41afee429d45620d8c7078f4fc521d58b18b3d1e
Artur <apmgor@gmail.com>
2023-10-23 00:02:27 +0300

    Add auto-create Kafka topic

--- product-service/src/main/java/ru/apmgor/productservice/config/ProducerConfig.java
+++ product-service/src/main/java/ru/apmgor/productservice/config/ProducerConfig.java
@@ -1,10 +1,12 @@
 package ru.apmgor.productservice.config;
 
 import lombok.val;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import reactor.core.publisher.Sinks;
 import reactor.kafka.sender.SenderOptions;
@@ -36,5 +38,13 @@ public class ProducerConfig {
         val sink = Sinks.many().unicast().<ProductViewEvent>onBackpressureBuffer();
         val flux = sink.asFlux();
         return new ProductViewEventProducer(template, sink, flux, topicName);
+    }
+
+    @Bean
+    public NewTopic createTopic() {
+        return TopicBuilder.name(topicName)
+                .partitions(3)
+                .replicas(3)
+                .build();
     }
 }
